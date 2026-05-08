301734823587fc585946ab5b7a0af1a1faa14fab
Dasun Batheegama <dasunbatheegama256@gmail.com>
2026-05-08 13:04:35 +0530

    chore: add Render URL to allowedHosts in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', 
+    allowedHosts: [
+      'todo-app-v1-o3d9.onrender.com'
+    ]
+  }
 })
